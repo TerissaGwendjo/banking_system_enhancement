f918b7042417dc1154127647e99e6256bbb111d5
dci-student <Terissa>
2024-05-22 15:55:10 +0200

    changes made in thymeleaf files

--- src/main/java/org/example/banking_system/repository/AccountRepository.java
+++ src/main/java/org/example/banking_system/repository/AccountRepository.java
@@ -5,6 +5,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+/**
+ * This interface extends JpaRepository provided by Spring Data JPA.
+ * JpaRepository provides basic CRUD (Create, Read, Update, Delete) operations
+ * for entities.
+ */
 public interface AccountRepository extends JpaRepository <Account, Long> {
     Account findByAccountNumber(String accountNumber);
+
+    /**
+     * This method finds an Account entity based on the provided account number.
+     * It leverages Spring Data JPA's findBy convention to automatically generate
+     * a query based on the method name and parameter.
+     *
+     * @param accountNumber The unique identifier for the account to be retrieved.
+     * @return An Account object if found, null otherwise.
+     */
 }
--- src/main/java/org/example/banking_system/service/AccountService.java
+++ src/main/java/org/example/banking_system/service/AccountService.java
@@ -1,8 +1,25 @@
 package org.example.banking_system.service;
 
+import org.springframework.stereotype.Service;
+
+
+/**
+ * This interface defines methods for managing accounts.
+ * It acts as a contract between the service implementation and the classes
+ * that will use it.
+ */
+@Service
 public interface AccountService {
+
+    //This method simulates opening a new account with a specific type and initial deposit.
     void openNewAccount(String accountType, double initialDeposit);
+
+    //This method simulates depositing money into an account identified by its number.
     void depositMoney (String accountNumber, double amount);
+
+    //This method simulates withdrawing money from an account identified by its number.
     void withdrawMoney (String accountNumber, double amount);
+
+    //This method simulates transferring money between two accounts identified by their numbers.
     void transferMoney (String fromAccount, String toAccount, double amount);
 }
