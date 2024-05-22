8a6af169123e60dc1fec79a9a2508f65a8941dae
dci-student <Terissa>
2024-05-22 11:09:29 +0200

    online banking system

--- src/main/java/org/example/banking_system/model/Account.java
+++ src/main/java/org/example/banking_system/model/Account.java
@@ -12,14 +12,57 @@ public class Account {
     @GeneratedValue (strategy = GenerationType.SEQUENCE)
     private Long id;
 
+    private String AccountNumber; // provided by the bank system and not ID
+    private String AccountType;
+    private Double balance;
 
+    //Constructors
 
+    public Account() {
+    }
 
+    public Account(Long id, String accountNumber, String accountType, Double balance) {
+        this.id = id;
+        AccountNumber = accountNumber;
+        AccountType = accountType;
+        this.balance = balance;
+    }
+
+    public Account(String accountNumber, String accountType, Double balance) {
+        AccountNumber = accountNumber;
+        AccountType = accountType;
+        this.balance = balance;
+    }
+
+    // Getters and setters
     public void setId(Long id) {
         this.id = id;
     }
 
     public Long getId() {
         return id;
+    }
+    public String getAccountNumber() {
+        return AccountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        AccountNumber = accountNumber;
+    }
+
+    public String getAccountType() {
+        return AccountType;
+    }
+
+    public void setAccountType(String accountType) {
+        AccountType = accountType;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
     }
 }
