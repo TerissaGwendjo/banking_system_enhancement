9689b2af554789b419056f190cc100e2b2e13f58
dci-student <Terissa>
2024-06-04 09:46:34 +0200

    UserController class updted

--- src/main/java/org/example/banking_system/controller/UserController.java
+++ src/main/java/org/example/banking_system/controller/UserController.java
@@ -4,6 +4,8 @@ import org.example.banking_system.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -19,6 +21,12 @@ public class UserController {
     @GetMapping("/signup")
     public String showRegistrationForm(){
         return "signup";
+    }
+
+    @PostMapping ("/signup")
+    public String signUp(@RequestParam String username,
+                         @RequestParam String password){
+        return "redirect:/login";
     }
 
 }
