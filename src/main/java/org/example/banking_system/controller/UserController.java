5b14a6374ad051cf1ade2d4b994b4add8d3bada3
dci-student <Terissa>
2024-06-04 09:08:23 +0200

    security classes enhanced

---
+++ src/main/java/org/example/banking_system/controller/UserController.java
@@ -0,0 +1,24 @@
+package org.example.banking_system.controller;
+
+import org.example.banking_system.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/login")
+    public String showLoginForm(){
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String showRegistrationForm(){
+        return "signup";
+    }
+
+}
