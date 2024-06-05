7877ffb41fead517a2cb7e1ed9967e337a9a85ce
dci-student <Terissa>
2024-06-05 10:34:17 +0200

    Banking Controller updated

--- src/main/java/org/example/banking_system/controller/BankingController.java
+++ src/main/java/org/example/banking_system/controller/BankingController.java
@@ -10,68 +10,68 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class BankingController {
 
-    @Autowired
-    private AccountService service;
+    @Autowired // Injects the AccountService bean into this controller
+    private AccountService accountService;
 
-    @GetMapping("/")
+    @GetMapping("/") // Maps the "/" URL to this method to show the home page
     public String showHomePage() {
-        return "home";
+        return "home"; // Returns the name of the home page template
     }
 
-    @GetMapping("/accounts/new")
+    @GetMapping("/accounts/new") // Maps the "/accounts/new" URL to this method to show the new account form
     public String showNewAccountForm (){
-        return "new-account";
+        return "new-account"; // Returns the name of the new account form template
     }
 
-    @PostMapping ("/accounts/new")
+    @PostMapping ("/accounts/new") // Maps the POST request to "/accounts/new" URL for opening a new account
     public String openNewAccountForm(
-            @RequestParam String accountType,
-            @RequestParam double initialDeposit
+            @RequestParam String accountType, // Retrieves the account type from the form
+            @RequestParam double initialDeposit // Retrieves the initial deposit amount from the form
     ) {
-        service.openNewAccount(accountType,initialDeposit);
-        return "redirect:/";
+        accountService.openNewAccount(accountType,initialDeposit); // Calls the AccountService to open a new account
+        return "redirect:/"; // Redirects the user to the home page after opening the account
     }
 
-    @GetMapping("/transactions/deposit")
+    @GetMapping("/transactions/deposit") // Maps the "/transactions/deposit" URL to this method to show the deposit form
     public String showDepositForm() {
-        return "deposit";
+        return "deposit"; // Returns the name of the deposit form template
     }
 
-    @PostMapping("/transactions/deposit")
+    @PostMapping("/transactions/deposit") // Maps the POST request to "/transactions/deposit" URL for depositing money
     public String depositMoney(
-            @RequestParam String accountNumber,
-            @RequestParam double amount
+            @RequestParam String accountNumber, // Retrieves the account number from the form
+            @RequestParam double amount // Retrieves the deposit amount from the form
     ) {
-        service.depositMoney(accountNumber, amount);
-        return "redirect:/";
+        accountService.depositMoney(accountNumber, amount); // Calls the AccountService to deposit money
+        return "redirect:/"; // Redirects the user to the home page after depositing money
     }
 
-    @GetMapping ("/transactions/withdraw")
+    @GetMapping ("/transactions/withdraw") // Maps the "/transactions/withdraw" URL to this method to show the withdrawal form
     public String showWithdrawForm (){
-        return "withdrawal";
+        return "withdrawal"; // Returns the name of the withdrawal form template
     }
 
-    @PostMapping ("/transactions/withdraw")
+    @PostMapping ("/transactions/withdraw") // Maps the POST request to "/transactions/withdraw" URL for withdrawing money
     public String withDrawMoney(
-            @RequestParam String accountNumber,
-            @RequestParam double amount
-    ) { service.withdrawMoney(accountNumber,amount);
-        return "redirect:/" ;
-
+            @RequestParam String accountNumber, // Retrieves the account number from the form
+            @RequestParam double amount // Retrieves the withdrawal amount from the form
+    ) {
+        accountService.withdrawMoney(accountNumber,amount); // Calls the AccountService to withdraw money
+        return "redirect:/"; // Redirects the user to the home page after withdrawing money
     }
 
-    @GetMapping ("/transactions/transfer")
+    @GetMapping ("/transactions/transfer") // Maps the "/transactions/transfer" URL to this method to show the transfer form
     public String showTransferForm (){
-        return "transfer";
+        return "transfer"; // Returns the name of the transfer form template
     }
 
-
-    @PostMapping ("/transactions/transfer")
-    public String transferMoney(@RequestParam String fromAccount,
-                                @RequestParam String toAccount,
-                                @RequestParam double amount){
-        service.transferMoney(fromAccount,toAccount,amount);
-        return "redirect:/";
-
+    @PostMapping ("/transactions/transfer") // Maps the POST request to "/transactions/transfer" URL for transferring money
+    public String transferMoney(
+            @RequestParam String fromAccount, // Retrieves the sender's account number from the form
+            @RequestParam String toAccount, // Retrieves the receiver's account number from the form
+            @RequestParam double amount // Retrieves the transfer amount from the form
+    ){
+        accountService.transferMoney(fromAccount,toAccount,amount); // Calls the AccountService to transfer money
+        return "redirect:/"; // Redirects the user to the home page after transferring money
     }
 }
